43d6e6a78cf6f038ab1b41fc60e973cd72af71fd
hungvqdev <hungvq.dev@gmail.com>
2022-10-21 02:37:35 +0700

    model Pizza

---
+++ models/pizza.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const pizzaSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  varients: [],
+  prices: [],
+  category: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  timestamps: true
+});
+
+module.exports = mongoose.model('Pizza', pizzaSchema);
