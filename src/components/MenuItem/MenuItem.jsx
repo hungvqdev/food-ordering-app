1f95889886711f2ac4931da615c2e81202fe8ed7
hungvqdev <hungvq.dev@gmail.com>
2022-10-22 02:37:38 +0700

    redux pizza

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -1,71 +1,102 @@
-import React, { useState } from 'react'
-import Modal from 'react-bootstrap/Modal';
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
 
-const MenuItem = ({pizza}) => {
-    const [ quantity, setQuanity ]  = useState(1)
-    const [ varient, setVarient ]   = useState('small')
+const MenuItem = ({ pizza }) => {
+  const [quantity, setQuanity] = useState(1);
+  const [varient, setVarient] = useState("small");
 
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
-    <div style={{margin: "30px"}} className='shadow-lg p-3 mb-5 bg-white rounded '>
-        <div onClick={handleShow}>
-            <p className='p__cormorant color_p text-center' style={{ color: '#222222'}}>{pizza.name}</p>
-            <img src={pizza.image} alt={pizza.name} className="img-fluid app__wrapper_info" style={{height:"280px", width:"280px"}} />
+    <div className="shadow-lg p-3 mb-5  rounded m-3">
+      <div onClick={handleShow}>
+        <p className="p__cormorant color_p text-center border border-light border-3 m-1">{pizza.name}</p>
+        <img
+          src={pizza.image}
+          alt={pizza.name}
+          className="img-fluid app__wrapper_info m-auto "
+          style={{ height: "280px", width: "280px" }}
+        />
+      </div>
+
+      <div className="border border-light border-3 p-1 mt-">
+        <div className="d-flex">
+          <div className="w-100 m-1">
+            <p className="p__opensans mt-2 p__menuItem">Kích cỡ</p>
+            <select
+              className="form-select"
+              value={varient}
+              onChange={(e) => {
+                setVarient(e.target.value);
+              }}
+            >
+              {pizza.varients.map((varient) => {
+                return <option value={varient}>{varient}</option>;
+              })}
+            </select>
+          </div>
+          <div className="w-100 m-1">
+            <p className="p__opensans mt-2 p__menuItem">Số Lượng</p>
+
+            <select
+              className="form-select"
+              value={quantity}
+              onChange={(e) => {
+                setQuanity(e.target.value);
+              }}
+            >
+              {[...Array(10).keys()].map((x, i) => {
+                return <option value={i + 1}>{i + 1}</option>;
+              })}
+            </select>
+          </div>
         </div>
-        <div className='d-flex'>
-            <div className='w-100 m-1'>
-            <p className="p__opensans mt-2 p__menuItem" style={{ color: '#222222'}}>Kích cỡ</p>
-                <select className='form-select' value={varient} onChange={(e) =>{setVarient(e.target.value)}}>
-                    {pizza.varients.map(varient => {
-                        return (
-                            <option value={varient}>{varient}</option>
-                        )
-                    })}
-                </select>
-                
-            </div>
-            <div className='w-100 m-1'>
-                <p className="p__opensans mt-2 p__menuItem" style={{ color: '#222222'}}>Số Lượng</p>
+        <div className="d-flex justify-content-between ">
+          <div className=" m-1">
+            <p className="p__cormorant color_p mt-2">
+              Giá: {pizza.prices[0][varient] * quantity}đ
+            </p>
+          </div>
 
-                <select className='form-select' value={quantity} onChange={(e) =>{setQuanity(e.target.value)}}>
-                    {[...Array(10).keys()].map((x , i) => {
-                        return (
-                            <option value={i+1}>{i+1}</option>
-                        )
-                    })}
-                </select>
-            </div>
+          <div className="  m-1">
+            <button className="custom__button">Thêm vào giỏ</button>
+          </div>
         </div>
-        <div className='d-flex justify-content-between'>
-      
-            <div className=' m-1'>
-                <p className='p__cormorant color_p mt-2' style={{ color: '#222222'}}>Giá: {pizza.prices[0][varient] * quantity}đ</p>
-            </div>
+      </div>
 
-            <div className='  m-1'>
-                <button className='custom__button'>Thêm vào giỏ</button>
-            </div>
-        </div>         
-        
-        <Modal show={show} onHide={handleClose} className="modal__Menuitem ">
-      <Modal.Header closeButton>
-        <Modal.Title><p className='p__cormorant color_p text-center' style={{ color: '#222222'}}>{pizza.name}</p></Modal.Title>
-      </Modal.Header>
+      <Modal show={show} onHide={handleClose} className="modal__Menuitem ">
+        <Modal.Header closeButton>
+          <Modal.Title>
+            <p
+              className="p__cormorant color_p mb-0"
+              style={{ color: "#222222" }}
+            >
+              {pizza.name}
+            </p>
+          </Modal.Title>
+        </Modal.Header>
 
-      <Modal.Body>
-        <p className='text-center'><img src={pizza.image} alt={pizza.name} className="img-fluid app__wrapper_info" style={{height:"350px", width:"350px"}} /></p>
-      
-      </Modal.Body>
-      <Modal.Footer>  
-       <p className='p__opensans' style={{ color: '#222222'}}>{pizza.description}</p>
-      </Modal.Footer>
-    </Modal>                
-
+        <Modal.Body>
+          <img
+            src={pizza.image}
+            alt={pizza.name}
+            className="img-fluid app__wrapper_info m-auto"
+            style={{ height: "350px", width: "350px" }}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <p
+            className="p__opensans m-auto mt-1 mb-1"
+            style={{ color: "#222222" }}
+          >
+            {pizza.description}
+          </p>
+        </Modal.Footer>
+      </Modal>
     </div>
-  )
-}
+  );
+};
 
-export default MenuItem
+export default MenuItem;
--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,29 +1,45 @@
-import React from 'react'
-import { data } from '../../constants'
-import { SubHeading, MenuItem } from '../../components';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllPizzas } from "../../actions/pizzaActions";
+import { data } from "../../constants";
+import { SubHeading, MenuItem } from "../../components";
 
 const Menu = () => {
-  return (
-    <div className='app__bg section__padding ' id="menu">
-        <div className="app__Menu-title  flex__center text-center">
-            <div className=''>
-            <SubHeading title="Thực đơn phù hợp với bạn" />
-           <h1 className="headtext__cormorant">Today&apos;s Special</h1>
-            </div>
-           
-        </div>
-        <div className='row'>
-            {data.pizzas.map(pizza => {
-                return (
-                    <div className='col-lg-4 col-md-6 col-sm-12'>            
-                    <MenuItem pizza={pizza}/>
-                    </div>
-                )
-                
-            })}
-        </div>    
-    </div>
-  )
-}
+  const dispatch = useDispatch();
 
-export default Menu
+  const pizzasState = useSelector((state) => state.getAllPizzasReducer);
+
+  const { pizzas, error, loading } = pizzasState;
+
+  useEffect(() => {
+    dispatch(getAllPizzas());
+  }, []);
+
+  return (
+    <div className="app__bg section__padding " id="menu">
+      <div className="app__Menu-title  flex__center text-center">
+        <div className="">
+          <SubHeading title="Thực đơn phù hợp với bạn" />
+          <h1 className="headtext__cormorant">Today&apos;s Special</h1>
+        </div>
+      </div>
+      <div className="row">
+        {loading ? (
+          <h1>Loading</h1>
+        ) : error ? (
+          <h1>Error</h1>
+        ) : (
+          pizzas.map((pizza) => {
+            return (
+              <div className="col-lg-4 col-md-6 col-sm-12"   key={pizza._id}>
+                    <MenuItem pizza={pizza} />
+              </div>
+            );
+          })
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
