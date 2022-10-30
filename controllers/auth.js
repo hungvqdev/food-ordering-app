61c2f30eba0ce0362c824cd76cf990d3cd8cd28f
hungvqdev <hungvq.dev@gmail.com>
2022-10-31 02:08:19 +0700

    Login

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,3 +13,27 @@ exports.postRegister = async (req, res, next) => {
         return res.status(400).json({ message: error})
     }
 }
+
+exports.postLogin = async (req, res, next) => {
+
+    const { email, password} = req.body
+
+    try {
+        const user = await User.find({email, password})
+
+        if(user.length > 0) {
+            const currentUser = {
+                name: user[0].name,
+                email: user[0].email,
+                isAdmin: user[0].isAdmin,
+                _id: user[0]._id
+            }
+            return res.send(currentUser)
+        }else {
+            return res.status(400).json({ messgage: "Đăng nhập thất bại"})
+        }
+        
+    } catch (error) {
+        return res.status(400).json({ messgage: error})
+    }
+}
--- routes/auth.js
+++ routes/auth.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const authController = require('../controllers/auth')
 
 router.post('/register', authController.postRegister)
+router.post('/login', authController.postLogin)
 
 module.exports = router;
