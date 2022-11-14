ad2699b1b438a3a8e61c8a92d155f81b133c03b0
hungvqdev <hungvq.dev@gmail.com>
2022-11-15 03:23:06 +0700

    add product

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,8 +4,6 @@ const Pizza = require('../models/pizza')
 exports.postDeleteProduct = async (req, res, next) => {
     const { prodId } = req.params
 
-    console.log(prodId)
-
     try {
         const product = await Pizza.deleteOne({ _id : prodId})
         return res.send(product)
@@ -21,12 +19,24 @@ exports.postEditProduct = async (req, res, next) => {
     const { prodId } = req.params
     const updates = { name: prod.name, category: prod.category, image: prod.image, prices: [{small : prod.small, medium: prod.medium, large: prod.large }], description: prod.description}
 
-    console.log(updates)
-
     try {
         const product = await Pizza.findByIdAndUpdate(prodId, updates, {new: true})
         res.send(product)
     } catch (error) {
         return res.status(400).json({ messgage: error})
+    }
+}
+
+exports.postAddProduct = async (req, res, next) => {
+    const {name, category, image, description, large, small, medium} = req.body
+    
+    data = {name: name, category: category, image: image, description: description, prices: [{ small: small, medium: medium, large: large}], varients: ['small','medium', 'large']}
+
+    try {
+        const newProduct = new Pizza(data)    
+        await newProduct.save()
+        res.send(newProduct)
+    } catch (error) {
+        return res.status(400).json({ message: error})
     }
 }
--- routes/admin.js
+++ routes/admin.js
@@ -5,5 +5,6 @@ const adminController = require('../controllers/admin')
 
 router.delete('/deleteproduct/:prodId', adminController.postDeleteProduct)
 router.patch('/editproduct/:prodId', adminController.postEditProduct)
+router.post('/addproduct', adminController.postAddProduct)
 
 module.exports = router
