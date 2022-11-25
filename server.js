1c28c2c585e816a8abe061d2b3a766a0df15ad7d
hungvqdev <hungvq.dev@gmail.com>
2022-11-25 20:42:43 +0700

    update

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const adminRouter = require('./routes/admin')
 
 app.use(cors());
 app.use(express.json())
-app.use('/', express.static(path.join(__dirname, 'dist')))
 
 app.use('/api/pizza/', pizzaRouter)
 app.use('/api/users/', authRouter)
