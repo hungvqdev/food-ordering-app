1028cdad49dbf01fd755a49a9c17ead1c7955b4d
hungvqdev <hungvq.dev@gmail.com>
2022-11-25 20:18:27 +0700

    make it better

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const db = require('./db')
 const cors = require('cors');
 const app = express()
 require('dotenv').config();
+const path = require('path')
 
 const pizzaRouter = require('./routes/pizza')
 const authRouter = require('./routes/auth')
@@ -10,13 +11,14 @@ const adminRouter = require('./routes/admin')
 
 app.use(cors());
 app.use(express.json())
+app.use('/', express.static(path.join(__dirname, 'dist')))
 
 app.use('/api/pizza/', pizzaRouter)
 app.use('/api/users/', authRouter)
 app.use('/api/admin/', adminRouter)
 
 app.get('/', (req, res) => {
-    res.send('server')
+    res.send('server is running')
 })
 
 const port = process.env.PORT || 8000
